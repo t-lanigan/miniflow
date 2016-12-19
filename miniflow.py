e58327f1a63cd759b465e05ff6ccc8a8057c26fb
Tyler Lanigan <tylerlanigan@gmail.com>
2016-12-19 14:57:51 -0800

    added MSE function

--- miniflow.py
+++ miniflow.py
@@ -53,6 +53,33 @@ class Sigmoid(Layer):
         input_value = self.inbound_layers[0].value
         self.value = self._sigmoid(input_value)
 
+class MSE(Layer):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last layer for a network.
+        """
+        # Call the base class' constructor.
+        Layer.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_layers[0].value.reshape(-1, 1)
+        a = self.inbound_layers[1].value.reshape(-1, 1)
+        m = len(y)
+        self.value = 1 / m * sum(np.square(y-a))
+
 class Linear(Layer):
     def __init__(self, inbound_layer, weights, bias):
         # Notice the ordering of the input layers passed to the
