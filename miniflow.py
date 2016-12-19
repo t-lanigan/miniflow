0aed481870ed4459ad43ac236589c063b4886301
Tyler Lanigan <tylerlanigan@gmail.com>
2016-12-19 13:38:12 -0800

    finished Linear Class

--- miniflow.py
+++ miniflow.py
@@ -70,6 +70,17 @@ class Mul(Neuron):
             result *= n.value
         self.value = result
 
+class Linear(Neuron):
+    def __init__(self, inputs, weights, bias):
+        Neuron.__init__(self, inputs)
+        
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other neurons.
+        # The weight and bias values are stored within the
+        # respective neurons.
+        self.weights = weights
+        self.bias = bias
+        
     def forward(self):
         self.value = self.bias.value
         for w, x in zip(self.weights, self.inbound_neurons):
